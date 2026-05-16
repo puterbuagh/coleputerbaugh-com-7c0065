3e91bddfae43cb7463f232494d4385a773185530
puterbaugh <126985461+puterbuagh@users.noreply.github.com>
2026-05-16 12:21:13 +0800

    Initial commit from Colable

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Hero from "@/components/home/Hero";
+import Packages from "@/components/home/Packages";
+import ProofStrip from "@/components/home/ProofStrip";
+import BioTeaser from "@/components/home/BioTeaser";
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <Packages />
+      <ProofStrip />
+      <BioTeaser />
+    </>
+  );
+}
+
+export default Home;
